286bbfce42245cba9e3f0f5ac09681996f2bb4e9
n-andr <150958639+n-andr@users.noreply.github.com>
2024-01-17 12:40:02 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,21 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
+	static char	read_line[BUFFER_SIZE];
+
+	read (fd, read_line, BUFFER_SIZE);
+	printf("%d#", read_line[2]);
+	printf("%d#", read_line[3]);
+		printf("%d#", read_line[25]);
+
 	
+	return (read_line);
 }
 
 
-#include <stdio.h>
+// #include <stdio.h>
 #include <fcntl.h>
 
 int	main()
@@ -20,15 +29,22 @@ int	main()
 	// dup2 (fd, 1);
 	// write (1, "hello", 5);
 
-	char	buf[256];
-	int		chars_read;
+	// char	buf[256];
+	// int	chars_read;
 
 	fd = open ("file.txt", O_RDWR);
-
-	while ((chars_read = read (fd, buf, 40)))
-	{
-		buf[chars_read] = '\0';
-		printf ("line 40 char: %s\n", buf);
-	}
+	printf ("%s\n", get_next_line(fd));
+	printf ("%s\n", get_next_line(fd));
+	printf ("%s\n", get_next_line(fd));
+	printf ("%s\n", get_next_line(fd));
+	printf ("%s\n", get_next_line(fd));
+	printf ("%s\n", get_next_line(fd));
+	// Writing all the content from file.txt 40 simbols at a time.
+	
+	// while ((chars_read = read (fd, buf, 40)))
+	// {
+	// 	buf[chars_read] = '\0';
+	// 	printf ("line 40 char: %s\n", buf);
+	// }
 	return (0);
 }
